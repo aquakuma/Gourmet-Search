620f8b17c0216d8dd64886056ccd804fe0c9319f
aquakuma <c1720176@hotmail.com>
2021-04-19 15:57:28 +0900

    recomiit

--- api.php
+++ api.php
@@ -35,42 +35,42 @@ function area(){
 }
 
 
-    $mode = "";
-    if(isset($_POST['mode'])){
-        if($_POST['mode'] == "local"){
-            $lat = $_POST['lat'];
-            $lng = $_POST['lng'];
-            $range = $_POST['range'];
-            $array = get_json($lat,$lng,$range);
-            header('Content-type: application/json');
-            echo json_encode($array,JSON_UNESCAPED_UNICODE, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
-        
-        }
-        if($_POST['mode'] == "search"){
-            $area = $_POST['area'];
-            $keyword = $_POST['keyword'];
-            $party_capacity = $_POST['party_capacity'];
-            if($_POST['flag'] == 1){
-                $needs = $_POST['needs'];
-            }
-            else{
-                $needs = [];
-            }
-            
-            $array = search($area,$keyword,$party_capacity,$needs);
-
-            header('Content-type: application/json');
-            echo json_encode($array,JSON_UNESCAPED_UNICODE, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
-        }
-
-        if($_POST['mode'] == "area"){
-
-            $array = area();
-
-            header('Content-type: application/json');
-            echo json_encode($array,JSON_UNESCAPED_UNICODE, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
-        }
+$mode = "";
+if(isset($_POST['mode'])){
+    if($_POST['mode'] == "local"){
+        $lat = $_POST['lat'];
+        $lng = $_POST['lng'];
+        $range = $_POST['range'];
+        $array = get_json($lat,$lng,$range);
+        header('Content-type: application/json');
+        echo json_encode($array,JSON_UNESCAPED_UNICODE, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+    
     }
+    if($_POST['mode'] == "search"){
+        $area = $_POST['area'];
+        $keyword = $_POST['keyword'];
+        $party_capacity = $_POST['party_capacity'];
+        if($_POST['flag'] == 1){
+            $needs = $_POST['needs'];
+        }
+        else{
+            $needs = [];
+        }
+        
+        $array = search($area,$keyword,$party_capacity,$needs);
+
+        header('Content-type: application/json');
+        echo json_encode($array,JSON_UNESCAPED_UNICODE, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+    }
+
+    if($_POST['mode'] == "area"){
+
+        $array = area();
+
+        header('Content-type: application/json');
+        echo json_encode($array,JSON_UNESCAPED_UNICODE, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+    }
+}
 
 
 
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let lat = 0;
 let lon = 0;
 var data_array = [];
 var shop_num = 0;
-
+area();
 function getPosition() {
     // 現在地を取得
 
@@ -57,119 +57,7 @@ function shop_list(lat, lng, range) {
     })
     // Ajax通信が成功した時
     .done( function(data) {
-        data_array = data;
-        shop_num = data['results_returned'];
-        console.log('通信成功');
-        console.log(data);
-        console.log("debug:" + data['results_returned']);
-        
-        var place = document.getElementById('place');
-        while (place.querySelector('div')) {
-            place.querySelector('div').remove();
-        }
-        while (place.querySelector('h3')) {
-            place.querySelector('h3').remove();
-        }
-
-        var result = document.createElement("div");
-        var details = document.createElement("div");
-
-        result.classList.add('items');
-
-
-        if (data['results_returned'] != 0) {
-            for (let i = 0; i < data['results_returned']; i++){
-                /* 
-                if (i % 4 == 0) {
-                    if (i != 0) {
-                        place.appendChild(div);
-                    }
-                    var div = document.createElement("div");
-                    div.classList.add('selection');
-                    
-                    let page = Math.floor(i / 4) + 1;
-                    console.log('page-' + String(page));
-                    div.id = 'page-' + String(page);
-                }
-                var name = document.createElement("h3");
-                name.textContent = data['shop'][i]['name'];
-                console.log(data['shop'][i]['name']);
-                div.appendChild(name);
-                */
-                
-                //店一覧
-                var child_div = document.createElement("div");
-                child_div.classList.add('item');
-                
-                var name = document.createElement("h3");
-                name.textContent = data['shop'][i]['name'];
-                console.log(data['shop'][i]['name']);
-
-                var img = document.createElement("img");
-                img.setAttribute('src', data['shop'][i]['photo']['pc']['l']);
-                img.setAttribute('alt', i);
-                img.setAttribute('onclick', `detail(${i})`); 
-
-                var p = document.createElement("p");
-                p.textContent = data['shop'][i]['access'];
-                child_div.appendChild(img);
-                child_div.appendChild(name);
-                child_div.appendChild(p);
-                result.appendChild(child_div);
-
-
-                //詳細画面
-
-                var child_div = document.createElement("div");
-                child_div.setAttribute('onclick', `detail_close(${i})`); 
-                child_div.classList.add('popup');
-                child_div.id = `shop-${i}`;
-                var content = document.createElement("div");
-                content.classList.add('content');
-
-                var h3 = document.createElement("h3");
-                h3.textContent = data['shop'][i]['name'];
-                content.appendChild(h3);
-
-                var img = document.createElement("img");
-                img.setAttribute('src', data['shop'][i]['photo']['pc']['l']);
-                content.appendChild(img);
-
-                var p = document.createElement("p");
-                p.textContent = "住所：" + data['shop'][i]['address'];
-                content.appendChild(p);
-
-                var p = document.createElement("p");
-                p.textContent =　"営業時間：" + data['shop'][i]['open'];
-                content.appendChild(p);
-
-  
-                /*
-                var close = document.createElement("a");
-                close.setAttribute('href', `javascript: detail_close(${i})`);
-                close.textContent = "閉じる";
-                content.appendChild(close);
-
-                */
-                
-                child_div.appendChild(content);
-                details.appendChild(child_div);
-
-                
-            }
-            place.appendChild(result);
-            place.appendChild(details);
-            $('.items').pagination({
-                itemElement : '> .item' // アイテムの要素
-            });
-  
-        }
-        else {
-            var h3 = document.createElement("h3");
-            h3.textContent = "検索結果はありません";
-            place.appendChild(h3);
-            console.log('検索結果はありません');
-        }
+        data_display(data);
             
     })
     // Ajax通信が失敗した時
@@ -220,119 +108,7 @@ function search() {
     })
     // Ajax通信が成功した時
     .done( function(data) {
-        data_array = data;
-        shop_num = data['results_returned'];
-        console.log('通信成功');
-        console.log(data);
-        console.log("debug:" + data['results_returned']);
-        
-        var place = document.getElementById('place');
-        while (place.querySelector('div')) {
-            place.querySelector('div').remove();
-        }
-        while (place.querySelector('h3')) {
-            place.querySelector('h3').remove();
-        }
-
-        var result = document.createElement("div");
-        var details = document.createElement("div");
-
-        result.classList.add('items');
-
-
-        if (data['results_returned'] != 0) {
-            for (let i = 0; i < data['results_returned']; i++){
-                /* 
-                if (i % 4 == 0) {
-                    if (i != 0) {
-                        place.appendChild(div);
-                    }
-                    var div = document.createElement("div");
-                    div.classList.add('selection');
-                    
-                    let page = Math.floor(i / 4) + 1;
-                    console.log('page-' + String(page));
-                    div.id = 'page-' + String(page);
-                }
-                var name = document.createElement("h3");
-                name.textContent = data['shop'][i]['name'];
-                console.log(data['shop'][i]['name']);
-                div.appendChild(name);
-                */
-                
-                //店一覧
-                var child_div = document.createElement("div");
-                child_div.classList.add('item');
-                
-                var name = document.createElement("h3");
-                name.textContent = data['shop'][i]['name'];
-                console.log(data['shop'][i]['name']);
-
-                var img = document.createElement("img");
-                img.setAttribute('src', data['shop'][i]['photo']['pc']['l']);
-                img.setAttribute('alt', i);
-                img.setAttribute('onclick', `detail(${i})`); 
-
-                var p = document.createElement("p");
-                p.textContent = data['shop'][i]['access'];
-                child_div.appendChild(img);
-                child_div.appendChild(name);
-                child_div.appendChild(p);
-                result.appendChild(child_div);
-
-
-                //詳細画面
-
-                var child_div = document.createElement("div");
-                child_div.setAttribute('onclick', `detail_close(${i})`); 
-                child_div.classList.add('popup');
-                child_div.id = `shop-${i}`;
-                var content = document.createElement("div");
-                content.classList.add('content');
-
-                var h3 = document.createElement("h3");
-                h3.textContent = data['shop'][i]['name'];
-                content.appendChild(h3);
-
-                var img = document.createElement("img");
-                img.setAttribute('src', data['shop'][i]['photo']['pc']['l']);
-                content.appendChild(img);
-
-                var p = document.createElement("p");
-                p.textContent = "住所：" + data['shop'][i]['address'];
-                content.appendChild(p);
-
-                var p = document.createElement("p");
-                p.textContent =　"営業時間：" + data['shop'][i]['open'];
-                content.appendChild(p);
-
-  
-                /*
-                var close = document.createElement("a");
-                close.setAttribute('href', `javascript: detail_close(${i})`);
-                close.textContent = "閉じる";
-                content.appendChild(close);
-
-                */
-                
-                child_div.appendChild(content);
-                details.appendChild(child_div);
-
-                
-            }
-            place.appendChild(result);
-            place.appendChild(details);
-            $('.items').pagination({
-                itemElement : '> .item' // アイテムの要素
-            });
-  
-        }
-        else {
-            var h3 = document.createElement("h3");
-            h3.textContent = "検索結果はありません";
-            place.appendChild(h3);
-            console.log('検索結果はありません');
-        }
+        data_display(data);
             
     })
     // Ajax通信が失敗した時
@@ -383,7 +159,189 @@ function area() {
 }
 
 
+//レスポンスのデータ表示
+function data_display(data){
+    data_array = data;
+        shop_num = data['results_returned'];
+        console.log('通信成功');
+        console.log(data);
+        console.log("debug:" + data['results_returned']);
+        
+        var place = document.getElementById('place');
+        while (place.querySelector('div')) {
+            place.querySelector('div').remove();
+        }
+        while (place.querySelector('h3')) {
+            place.querySelector('h3').remove();
+        }
 
+        var result = document.createElement("div");
+        var details = document.createElement("div");
+
+        result.classList.add('items');
+
+
+        if (data['results_returned'] != 0) {
+            for (let i = 0; i < data['results_returned']; i++){
+                /* 
+                if (i % 4 == 0) {
+                    if (i != 0) {
+                        place.appendChild(div);
+                    }
+                    var div = document.createElement("div");
+                    div.classList.add('selection');
+                    
+                    let page = Math.floor(i / 4) + 1;
+                    console.log('page-' + String(page));
+                    div.id = 'page-' + String(page);
+                }
+                var name = document.createElement("h3");
+                name.textContent = data['shop'][i]['name'];
+                console.log(data['shop'][i]['name']);
+                div.appendChild(name);
+                */
+                
+                //店一覧
+                var child_div = document.createElement("div");
+                child_div.classList.add('item');
+                
+
+
+                var img = document.createElement("img");
+                img.setAttribute('src', data['shop'][i]['photo']['pc']['l']);
+                img.setAttribute('alt', i);
+                img.setAttribute('onclick', `detail(${i})`); 
+                child_div.appendChild(img);
+
+                var name = document.createElement("h3");
+                name.textContent = data['shop'][i]['name'];
+                console.log(data['shop'][i]['name']);
+                child_div.appendChild(name);
+
+                var p = document.createElement("p");
+                p.textContent = data['shop'][i]['access'];
+                child_div.appendChild(p);
+
+                
+                result.appendChild(child_div);
+
+
+                //詳細画面
+
+                var child_div = document.createElement("div");
+                child_div.setAttribute('onclick', `detail_close(${i})`); 
+                child_div.classList.add('popup');
+                child_div.id = `shop-${i}`;
+                var content = document.createElement("div");
+                content.classList.add('content');
+
+                var h3 = document.createElement("h3");
+                h3.textContent = data['shop'][i]['name'];
+                content.appendChild(h3);
+
+                var div = document.createElement("div");
+                div.style.textAlign = "center";
+                var img = document.createElement("img");
+                img.setAttribute('src', data['shop'][i]['photo']['pc']['l']);
+                div.appendChild(img);
+                content.appendChild(div);
+
+                var detail_area = document.createElement("div");
+                detail_area.classList.add('detail_area');
+
+                var table = document.createElement("table");
+                table.classList.add('detail_table');
+
+
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.classList.add('td1');
+                td.textContent = "アクセス";
+                tr.appendChild(td);
+                var td = document.createElement("td");
+                td.textContent = data['shop'][i]['access'];
+                tr.appendChild(td);
+                table.appendChild(tr);
+
+                
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.classList.add('td1');
+                td.textContent = "住所";
+                tr.appendChild(td);
+                var td = document.createElement("td");
+                td.textContent = data['shop'][i]['address'];
+                tr.appendChild(td);
+                table.appendChild(tr);
+
+
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.classList.add('td1');
+                td.textContent = "営業時間";
+                tr.appendChild(td);
+                var td = document.createElement("td");
+                td.textContent = data['shop'][i]['open'];
+                tr.appendChild(td);
+                table.appendChild(tr);
+
+
+                
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.classList.add('td1');
+                td.textContent = "料金";
+                tr.appendChild(td);
+                var td = document.createElement("td");
+                td.textContent = data['shop'][i]['budget']['average'];
+                tr.appendChild(td);
+                table.appendChild(tr);
+
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.classList.add('td1');
+                td.textContent = "予約ページ";
+                tr.appendChild(td);
+                var td = document.createElement("td");
+                var a = document.createElement("a");
+                a.setAttribute('href', data['shop'][i]['urls']['pc']);
+                a.setAttribute('target', "_blank");
+                a.setAttribute('ref', "noopener noreferrer");
+                a.textContent = data['shop'][i]['urls']['pc'];
+                td.appendChild(a);
+                tr.appendChild(td);
+                table.appendChild(tr);
+
+
+                detail_area.appendChild(table);
+                content.appendChild(detail_area);
+                /*
+                var close = document.createElement("a");
+                close.setAttribute('href', `javascript: detail_close(${i})`);
+                close.textContent = "閉じる";
+                content.appendChild(close);
+
+                */
+                
+                child_div.appendChild(content);
+                details.appendChild(child_div);
+
+                
+            }
+            place.appendChild(result);
+            place.appendChild(details);
+            $('.items').pagination({
+                itemElement : '> .item' // アイテムの要素
+            });
+  
+        }
+        else {
+            var h3 = document.createElement("h3");
+            h3.textContent = "検索結果はありません";
+            place.appendChild(h3);
+            console.log('検索結果はありません');
+        }
+}
 
 
 
@@ -426,7 +384,31 @@ function detail_close(i) {
 
 
 
-window.addEventListener('DOMContentLoaded', area);
+function tag_change(i){
+    tag_array = ["box1","box2"];
+    tag = document.getElementById(i);
+    tag_array.forEach(function(element, index){
+        if(index == i){
+            content = document.getElementById(element);
+            content.style.display ="block";
+
+            tag = document.getElementById("tag"+String(index+1));
+            tag.classList.add('tag_current');
+        }
+        else{
+            content = document.getElementById(element);
+            content.style.display ="none";
+
+            tag = document.getElementById("tag"+String(index+1));
+            tag.classList.remove('tag_current');
+        }
+    });
+    
+}
+
+
+
+//window.addEventListener('DOMContentLoaded', area);
 
 
 
